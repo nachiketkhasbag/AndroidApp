faf14f5fec825a80ff6aaba32742f89c3478e429
nachiketkhasbag <nachiketkhasbag@gmail.com>
2018-06-20 01:07:34 -0700

    Enable forgotpassword functionality

--- app/src/main/java/com/khasna/cooker/Common/ForgotPassword.java
+++ app/src/main/java/com/khasna/cooker/Common/ForgotPassword.java
@@ -2,12 +2,17 @@ package com.khasna.cooker.Common;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.khasna.cooker.R;
 
 /**
@@ -24,6 +29,7 @@ public class ForgotPassword extends AppCompatActivity {
         setContentView(R.layout.activity_forgotpassword);
 
         final Button resetPasswordButton = (Button)findViewById(R.id.resetPasswordButton);   // FragmentResetPasswordChef button listener
+        final TextView emailTextView = (TextView)findViewById(R.id.email);   // FragmentResetPasswordChef button listener
 
         resetPasswordButton.setOnClickListener(new View.OnClickListener() {
 
@@ -32,11 +38,25 @@ public class ForgotPassword extends AppCompatActivity {
 
                 String errorCode = ValidateInput();
 
-                if (errorCode.compareTo("VALID_INPUT") != 0)
+                if (errorCode.compareTo("VALID_INPUT") == 0)
                 {
                     // Write your code here
 //                    debugClass.DebugPrint("InForgotPassword", errorCode);
-                    Toast.makeText(getApplicationContext(),"Invalid Input",Toast.LENGTH_LONG).show();
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    auth.sendPasswordResetEmail(emailTextView.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(),"Reset email sent to " + emailTextView.getText().toString() ,
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),"Error" + task.getException(),Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
                 }
                 else
                 {
